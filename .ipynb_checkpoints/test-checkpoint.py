650de9ad919cdf704cfbd483a5c3c8924f4b68ff
Sergio Vilela <jose.sergio.vilela@tecnico.ulisboa.pt>
2025-10-17 15:09:54 +0100

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.  Changes to be committed:     Setup virtual environment and jupyter notebook for data science project

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+
+print("Hello World")
+
+path = "C:\\Users\\LENOVO\\Documents\\Projetos\\Data_science\\data_science\\cybersec_dataset\\cybersecurity_attacks.csv"
+df = pd.read_csv(path)
+
+print(df.head())
+
+
--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+# C:/Users/LENOVO/Documents/Projetos/Data_science/data_science/env/Scripts/Activate.ps1 
+import pandas as pd
+
 print("Hello World")
+
+path = "C:\\Users\\LENOVO\\Documents\\Projetos\\Data_science\\cybersec_dataset\\cybersecurity_attacks.csv"
+df = pd.read_csv(path)
+
+print(df.head())
+
 
